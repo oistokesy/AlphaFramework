b953c3e50798dabe2c2dc16c05309f8b3a1c008f
oistokesy <stokesydoescoding@gmail.com>
2017-01-27 11:19:13 +0000

    added logincontroller and model

---
+++ Models/loginModel.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: terry
+ * Date: 27/01/17
+ * Time: 11:11
+ */
+class loginModel
+{
+
+}
---
+++ controllers/loginController.php
@@ -0,0 +1,7 @@
+gitt<?php
+/**
+ * Created by PhpStorm.
+ * User: terry
+ * Date: 27/01/17
+ * Time: 11:18
+ */
